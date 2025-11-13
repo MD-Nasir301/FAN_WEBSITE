216ab6807764358852ca567f86b24807388f7b52
nasir21b@gmail.com <nasir21b@gmail.com>
2025-11-14 02:32:38 +0600

    Almost done Table Fan

---
+++ main.js
@@ -0,0 +1,28 @@
+let fanStart = gsap.to(".fan", {
+  rotation: 360000,
+  duration: 100,
+  repeat: -1,
+  ease: "none",
+});
+gsap.to(".logo-bg", {
+  backgroundColor: "yellow",
+  yoyo: true,
+  duration: 3,
+  repeat: -1,
+  ease: "none",
+});
+
+fanStart.pause();
+
+let suiweOff = document.querySelector(".offbtn");
+let suiweOn = document.querySelector(".onbtn");
+
+document.querySelector(".onbtn").addEventListener("click", () => {
+  fanStart.resume();
+  suiweOff.style.zIndex = "15";
+});
+
+document.querySelector(".offbtn").addEventListener("click", () => {
+  fanStart.pause();
+  suiweOff.style.zIndex = "0";
+});
